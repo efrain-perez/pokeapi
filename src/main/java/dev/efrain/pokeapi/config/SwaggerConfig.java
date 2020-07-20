fd6567945753cb1345df7fec8b3cee65e3d8758c
Efrain Perez <efrain.p93@gmail.com>
2020-07-19 20:40:20 -0500

    Improves swagger config

--- src/main/java/dev/efrain/pokeapi/config/SwaggerConfig.java
+++ src/main/java/dev/efrain/pokeapi/config/SwaggerConfig.java
@@ -2,15 +2,17 @@ package dev.efrain.pokeapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -21,10 +23,25 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.basePackage("dev.efrain.pokeapi"))
                 .paths(PathSelectors.any())
                 .build()
+                .apiInfo(getApiInfo())
                 .useDefaultResponseMessages(false);
+    }
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Poke API",
+                "This API does not intend to be a substitute of the original <a " +
+                        "href=\"https://pokeapi.co\">PokeAPI</a> but instead provide further information using that " +
+                        "same API.",
+                "1.0",
+                "",
+                new Contact("Efrain Perez","https://efrain.dev","efrain.p93@gmail.com"),
+                "MIT",
+                "https://github.com/failo/pokeapi/blob/master/LICENSE",
+                Collections.emptyList()
+        );
     }
 
     @Override
